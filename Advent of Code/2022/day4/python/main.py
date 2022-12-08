70ed7d2e16f262945be63ba291538db974f0a137
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2022-12-08 14:49:36 +0000

    Add files via upload

---
+++ Advent of Code/2022/day4/python/main.py
@@ -0,0 +1,20 @@
+input = open("puzzle_input.txt", "r")
+
+elves_groups = input.read().splitlines()
+total_groups = len(elves_groups)
+futilities = 0
+overlaps = 0
+
+for i in range(total_groups):
+    current_group = elves_groups[i].split(",")
+    elf1 = [eval(i) for i in (current_group[0].split("-"))]
+    elf2 = [eval(i) for i in (current_group[1].split("-"))]
+    
+    if elf1[0] <= elf2[0] and elf1[1] >= elf2[1] or elf2[0] <= elf1[0] and elf2[1] >= elf1[1]:
+        futilities += 1
+        
+    if set(range(elf1[0], elf1[1]+1)) & set(range(elf2[0], elf2[1]+1)):
+        overlaps += 1
+        
+print("futilities:\t",futilities)
+print("overlaps:\t",overlaps)

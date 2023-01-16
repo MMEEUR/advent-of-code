7fc7b4e536dd37207e834d98e0f42a950eb139a7
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-01-16 20:45:35 +0330

    Day 12 Answer

---
+++ 2022/day12/python/part1/main.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+mountains = [list(x) for x in open('../puzzle_input.txt').read().strip().splitlines()]
+
+for row, i in enumerate(mountains):
+    for column, mt in enumerate(i):
+        if mt == 'S':
+            start_row = row
+            start_column = column
+            mountains[row][column] = 'a'
+        if mt == 'E':
+            end_row = row
+            end_column = column
+            mountains[row][column] = 'z'
+
+path = deque()
+path.append((0, start_row, start_column))
+
+back = {(start_row, start_column)}
+
+while path:
+    steps, row, column = path.popleft()
+    for near_row, near_column in [(row + 1, column), (row - 1, column), (row, column + 1), (row, column - 1)]:
+        if near_row < 0 or near_column < 0 or near_row >= len(mountains) or near_column >= len(mountains[0]):
+            continue
+        if (near_row, near_column) in back:
+            continue
+        if ord(mountains[near_row][near_column]) - ord(mountains[row][column]) > 1:
+            continue
+        if near_row == end_row and near_column == end_column:
+            print(steps + 1)
+            exit(0)
+        back.add((near_row, near_column))
+        path.append((steps + 1, near_row, near_column))

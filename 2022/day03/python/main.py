1b595daf80d45621d1f55867f106fe9ad83b6798
Mahdyar <mmeeur@gmail.com>
2023-04-14 21:37:02 +0000

    Optimize day 3 answer

--- 2022/day03/python/main.py
+++ 2022/day03/python/main.py
@@ -1,40 +1,21 @@
-rucksacks = open("puzzle_input.txt").read().splitlines()
-rucksacks2 = [rucksacks[i:i+3] for i in range(0, len(rucksacks), 3)]
-
-def puzzleSolver():
-    priorities = 0
+with open("puzzle_input.txt") as f:
+    inputs = f.read().splitlines()
     
-    for i in range(len(rucksacks)):
-        current_rucksack = rucksacks[i]
-        length = len(current_rucksack) // 2
-        current_rucksack1 = current_rucksack[:length]
-        current_rucksack2 = current_rucksack[length:]
-        common_word, = set(current_rucksack1) & set(current_rucksack2)
+def solver(part: bool, a: str, b: str = None, c: str = None):
+    
+    if part:
         
-        if common_word >= "a":
-            priorities += ord(common_word) - ord("a") + 1
-        else:
-            priorities += ord(common_word) - ord("A") + 27
-            
-    print(priorities)
-    
-def puzzleSolver2():
-    priorities = 0
-    
-    for i in range(len(rucksacks2)):
-        current_rucksack = rucksacks2[i]
-        current_rucksack1 = current_rucksack[0]
-        current_rucksack2 = current_rucksack[1]
-        current_rucksack3 = current_rucksack[2]
-
-        common_word, = set(current_rucksack1) & set(current_rucksack2) & set(current_rucksack3)
+        x: int = len(a) // 2
         
-        if common_word >= "a":
-            priorities += ord(common_word) - ord("a") + 1
-        else:
-            priorities += ord(common_word) - ord("A") + 27
+        common, = set(a[x:]) & set(a[:x])
+        
+        return ord(common) - 96 if common >= "a" else ord(common) - 38
+        
+    else:
+        
+        common, = set(a) & set(b) & set(c)
+        
+        return ord(common) - 96 if common >= "a" else ord(common) - 38
 
-    print(priorities)
-    
-puzzleSolver()
-puzzleSolver2()
+print(sum(solver(True, a) for a in inputs))
+print(sum(solver(False, *inputs[i:i+3]) for i in range(0, len(inputs), 3)))

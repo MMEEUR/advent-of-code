ff859a1677fa79083743dc573cb6c8caadee6db0
Mahdyar <mmeeur@gmail.com>
2023-04-14 19:10:45 +0000

    Optimize day 1 answer

--- 2022/day01/python/main.py
+++ 2022/day01/python/main.py
@@ -1,6 +1,8 @@
 import itertools
 
-inputs = open("puzzle_input.txt", "r").read().strip().splitlines()
+with open("puzzle_input.txt", "r") as f:
+
+    inputs = f.read().strip().splitlines()
 
 calories = [sum(map(int, g)) for k, g in itertools.groupby(inputs, bool) if k]
         

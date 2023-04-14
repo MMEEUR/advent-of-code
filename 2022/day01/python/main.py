a3dd47ee8bfef062ab247b52f4abc83adc9c7b22
Mahdyar <mmeeur@gmail.com>
2023-04-14 20:36:53 +0000

    Optimize day 1 answer

--- 2022/day01/python/main.py
+++ 2022/day01/python/main.py
@@ -6,12 +6,5 @@ with open("puzzle_input.txt", "r") as f:
 
 calories = [sum(map(int, g)) for k, g in itertools.groupby(inputs, bool) if k]
         
-# Part1
-
 print(max(calories))
-
-# Part2
-
-calories.sort()
-
-print(sum(calories[-3:]))
+print(sum(sorted(calories)[-3:]))

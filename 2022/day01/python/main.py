10a4b0d67c6c512a085144cf7c6388d4ad5d02db
Mahdyar <mmeeur@gmail.com>
2023-04-16 18:39:27 +0000

    Optimize day 1 answer

--- 2022/day01/python/main.py
+++ 2022/day01/python/main.py
@@ -1,10 +1,11 @@
 import itertools
+from typing import List
 
 with open("puzzle_input.txt", "r") as f:
 
-    calories = f.read().strip().splitlines()
+    calories: List[str] = f.read().strip().splitlines()
 
-calories = [sum(map(int, g)) for k, g in itertools.groupby(calories, bool) if k]
+calories: List[int] = [sum(map(int, g)) for k, g in itertools.groupby(calories, bool) if k]
         
 print(max(calories))
 print(sum(sorted(calories)[-3:]))
